d93b6597ede31261239cf113d077a38e72c1e969
Victor Kushnir <kushnir.victor@gmail.com>
2021-11-08 23:17:10 +0200

    .

---
+++ Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class BasicMovement : MonoBehaviour
+{
+    public Animator animator;
+
+    public GameOverScreen GameOverScreen;
+    public static bool gameIsPaused;
+    public float moveSpeed;
+    public Rigidbody2D rb;
+    private Vector3 moveDirection;
+    [SerializeField] Slider HP;
+    void Start()
+    {
+        HP.value = 100;
+    }
+    public void GameOver()
+    {
+        PauseGame();
+        GameOverScreen.Setup();
+    }
+    //* Updates every frame.
+    void Update()
+    {
+        ProcessInputs();
+    }
+    //* Updates every fixed amount of frames.
+    void FixedUpdate()
+    {
+        Move();
+    }
+    //* Calculating the changes for the position and swaps the image of the players image by X axis respectivly.
+    void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        moveDirection = new Vector3(moveX, moveY,0.0f);
+        animator.SetFloat("Horizontal", moveDirection.x);
+        animator.SetFloat("Vertical", moveDirection.y);
+        animator.SetFloat("Magnitude", moveDirection.magnitude);
+        //* (.normalized) was added so the movement in 2 axis simultaneously wont double the speed . 
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            gameIsPaused = !gameIsPaused;
+            PauseGame();
+        }
+        
+    }
+    void PauseGame()
+    {
+        if (gameIsPaused)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+    //*Calculates the velocity of the the player using its position and Unity added speed.
+    void Move()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            if (HP.value <= 15)
+            {
+                GameOver();
+            }
+            HP.value -= 15;
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] float speed;
+    GameObject player;
+    Animator anim;
+    bool isAlive = true;
+    //*Called before the first frame update, determines the target which is the player and its walking and destruction animation .
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    //*Giving players coordinates to the enemy so he will follow him.
+    void Update()
+    {
+        if (player != null && isAlive)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.transform.position,speed*Time.deltaTime);
+        }
+    }
+    //*When players bullet touches the enemy it changes its animation and sets its off.
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Bullet"))
+        {
+            anim.SetTrigger("Boom1");
+            isAlive = false;
+            //*0.267 is the time that optimally fitts the animation.
+            Destroy(gameObject, 0.267f);
+        }
+    }
+
+}
